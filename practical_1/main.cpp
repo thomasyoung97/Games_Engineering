02817a92c1da42644db832dd6228a70ab9638744
unknown <thomas.young1997@outlook.com>
2018-01-23 16:43:24 +0000

    Pong set up done

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,26 +1,93 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+using namespace std;
+
+const Keyboard::Key controls[4] =
+{
+	Keyboard::A, // player1 UP
+	Keyboard::Z, // player1 Down
+	Keyboard::Up, //player2 Up
+	Keyboard::Down // Player2 Down
+};
+
+const Vector2f paddleSize(25.f, 100.f);
+const float ballRadius = 10.f;
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float paddleSpeed = 400.f;
+
+CircleShape ball;
+RectangleShape paddles[2];
+
+void load()
+{
+	// set size and origin of paddles
+	for (auto &p : paddles)
+	{
+		p.setSize(paddleSize - Vector2f(3, 3));
+		p.setOrigin(paddleSize / 2.f);
+	}
+
+	// set size and origin or ball
+	ball.setRadius(ballRadius - 3);
+	ball.setOrigin(ballRadius / 2, ballRadius / 2);
+
+	//reset paddle position
+	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+	paddles[1].setPosition((gameWidth - 10) - paddleSize.x / 2, gameHeight / 2);
+
+	ball.setPosition(gameWidth / 2 , gameHeight /2 );
+}
+
+void Update(RenderWindow &window)
+{
+	// reset clock , recalculate delta time
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	Event event;
+	while (window.pollEvent(event)) 
+	{
+		if (event.type == Event::Closed)
+		{
+			window.close();
+			return;
+		}
+	}
+
+	if (Keyboard::isKeyPressed(Keyboard::Escape))
+	{
+		window.close();
+	}
+
+	//handle paddle movement
+	float direction = 0.0f;
+	if (Keyboard::isKeyPressed(controls[0]))
+	{
+		direction--;
+	}
+	if (Keyboard::isKeyPressed(controls[1]))
+	{
+		direction++;
+	}
+	paddles[0].move(0, direction * paddleSpeed * dt);
+}
+void Render(RenderWindow &window)
+{
+	window.draw(paddles[0]);
+	window.draw(paddles[1]);
+	window.draw(ball);
+}
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200,200),"SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
+	load();
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
-		
-	window.clear();
-	window.draw(shape);
-	window.display(); 
-	
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
 	}
 	return 0;
 }
